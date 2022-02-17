914bbe5b8ce0f1bf9c378f0b1c3eb0263f77e4ce
unknown <ainur.akhmadishin@gmail.com>
2022-02-17 09:47:39 +0500

    add file

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import { store } from './app/store';
 import App from './app';
 
 render(
-  <BrowserRouter basename={process.env.PUBLIC_UR}>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Provider store={store}>
       <App />
     </Provider>
