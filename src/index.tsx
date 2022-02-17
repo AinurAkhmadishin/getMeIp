631920910a8a25f0e3b51fb405a8bcca10f3907b
AinurAkhmadishin <99537883+AinurAkhmadishin@users.noreply.github.com>
2022-02-17 10:28:49 +0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import { store } from './app/store';
 import App from './app';
 
 render(
-  <BrowserRouter basename={process.env.REACT_APP_API_PUBLIC}>
+  <BrowserRouter basename='https://get-me-ip.vercel.app'>
     <Provider store={store}>
       <App />
     </Provider>
