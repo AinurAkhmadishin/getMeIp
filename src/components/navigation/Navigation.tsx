a915b7b5ab0d39fa66f217e4d893c1fb6dc97260
unknown <ainur.akhmadishin@gmail.com>
2022-02-18 11:38:27 +0500

    add

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
-import { HashRouter, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './navigation.scss';
 
 export const Navigation = () => {
   return (
-    <HashRouter>
-      <div className="nav">
-        <NavLink to="/" className="nav__link">
-          Узнать данные по IP
-        </NavLink>
+    <div className="nav">
+      <NavLink to="/" className="nav__link">
+        Узнать данные по IP
+      </NavLink>
 
-        <NavLink to="/myIP" className="nav__link">
-          Свой IP
-        </NavLink>
-      </div>
-    </HashRouter>
+      <NavLink to="/myIP" className="nav__link">
+        Свой IP
+      </NavLink>
+    </div>
   );
 };
